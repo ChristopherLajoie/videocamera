1985d9c06e89f97c4f67eb01b0f5e562123f3503
ChristopherLajoie <lajc1503@usherbrooke.ca>
2024-04-30 19:02:40 -0400

    added ip input

--- src/ip_camera/gtk/gui.py
+++ src/ip_camera/gtk/gui.py
@@ -1,49 +1,78 @@
 import gi
 gi.require_version('Gtk', '3.0')
-gi.require_version('Gst', '1.0')
-from gi.repository import Gtk, Gst
+from gi.repository import Gtk
 
-Gst.init(None)  
-Gtk.init(None)
+def create_pipeline(ip_number):
+    pipeline = f"rtspsrc location=rtsp://192.168.144.{ip_number}:8554/main.264 latency=100 ! queue ! decodebin ! videoconvert ! gtksink sync=false name=video_sink"
+    return pipeline
 
-def stop_pipeline(button):
-    pipeline.set_state(Gst.State.NULL)
+class IPInputWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="IP Input")
+        self.set_default_size(200, 100)
 
-# Gtk
-win = Gtk.Window(title="Video Window")
-win.connect("destroy", Gtk.main_quit)
-win.set_default_size(600, 400)
+        self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.add(self.box)
 
-box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-win.add(box)
+        self.ip_entry = Gtk.Entry()
+        self.ip_entry.set_text("Enter IP Number")
+        self.box.pack_start(self.ip_entry, True, True, 0)
+
+        self.start_button = Gtk.Button.new_with_label("Start")
+        self.start_button.connect("clicked", self.on_start_clicked)
+        self.box.pack_start(self.start_button, True, True, 0)
+
+    def on_start_clicked(self, button):
+        ip_number = self.ip_entry.get_text()
+        pipeline_string = create_pipeline(ip_number)
+        main_window = MainWindow(pipeline_string)
+        main_window.show_all()
+        self.hide()
+
+class MainWindow(Gtk.Window):
+    def __init__(self, pipeline_string):
+        Gtk.Window.__init__(self, title="Video Window")
+        self.set_default_size(600, 400)
+
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.add(box)
+
+        self.pipeline = Gst.parse_launch(pipeline_string)
+
+        video_sink = self.pipeline.get_by_name("video_sink")
+        video_widget = video_sink.get_property("widget")
+
+        parent_container = video_widget.get_parent()
+
+        if parent_container:
+            parent_container.remove(video_widget)
+
+        box.pack_start(video_widget, True, True, 0)
+
+        self.pipeline.set_state(Gst.State.PLAYING)
+
+        self.stop_button = Gtk.Button.new_with_label("Stop")
+        self.stop_button.connect("clicked", self.on_stop_clicked)
+        box.pack_start(self.stop_button, False, False, 0)
+
+        self.connect("destroy", self.on_destroy)
+
+    def on_stop_clicked(self, button):
+        self.pipeline.set_state(Gst.State.NULL)
+        Gtk.main_quit()
+
+    def on_destroy(self, window):
+        self.pipeline.set_state(Gst.State.NULL)
+        Gtk.main_quit()
 
 # Gst
-pipeline = None  
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst
+Gst.init(None)
 
-def create_pipeline():
-    global pipeline
-    if pipeline:
-        stop_pipeline(None)  # Stop the existing pipeline before creating a new one
-    pipeline = Gst.parse_launch("rtspsrc location=rtsp://192.168.144.25:8554/main.264 latency=100 ! queue ! decodebin ! videoconvert ! gtksink sync=false name=video_sink")
+# Create IP input window
+ip_input_window = IPInputWindow()
+ip_input_window.connect("delete-event", Gtk.main_quit)
+ip_input_window.show_all()
 
-    video_sink = pipeline.get_by_name("video_sink")
-    video_widget = video_sink.get_property("widget")
-
-    parent_container = video_widget.get_parent()
-
-    if parent_container:
-        parent_container.remove(video_widget)
-
-    box.pack_start(video_widget, True, True, 0)
-
-    pipeline.set_state(Gst.State.PLAYING)
-    
-create_pipeline()  
-
-stop_button = Gtk.Button.new_with_label("Stop")
-stop_button.connect("clicked", stop_pipeline)
-box.pack_start(stop_button, False, False, 0)
-
-
-win.show_all()
 Gtk.main()
